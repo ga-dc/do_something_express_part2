f58148bb27fa4091d1352db1bca8195b4623948f
Rosel Padilla <roselpadilla.mail@gmail.com>
2015-08-19 08:58:28 -0400

    final

---
+++ app/models/tasks.js
@@ -0,0 +1,28 @@
+var exports = module.exports = {};
+exports.tasks = [
+  {
+    id: 0,
+    body: "Do better",
+    completed: false
+  },
+  {
+    id: 1,
+    body: "Be better",
+    completed: false
+  },
+  {
+    id: 2,
+    body: "Read",
+    completed: false
+  },
+  {
+    id: 3,
+    body: "Write",
+    completed: false
+  },
+  {
+    id: 4,
+    body: "Meditate",
+    completed: false
+  }
+];
