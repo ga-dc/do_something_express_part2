cd46847180e12eabfd7d0e3c904ac1b357cd46bc
lanak1301 <musiconstellation@gmail.com>
2015-08-18 21:10:33 -0400

    module exports setup

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express'),
   app = exports.app = express();
 
-  app.get("/", function(req, res){
-    res.send("hello world")
-  })
+var list = require("./app/models/list.js");
+var task = require("./app/models/task.js");
+var listsController = require("./app/controllers/listsController.js");
+var tasksController = require("./app/controllers/tasksController.js");
+
+  app.get("/", function(request, response){
+  response.sendFile(__dirname + "/app/views/index.html");
+  });
+
+  list.sayHello()
+  task.sayHello()
+  listsController.sayHello()
+  tasksController.sayHello()
 
   app.listen(4000, function(){
     console.log("app listening on port 4000")
