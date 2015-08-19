45790c6af95ef144240747fb0919bb7f99f3e3f7
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-08-18 21:43:18 -0400

    final submission

--- app/controllers/taskscontroller.js
+++ app/controllers/taskscontroller.js
@@ -1,55 +1,54 @@
--var tasks = [
--  {id: 1, body: "firsttodo", completed: false},
--  {id: 2, body: "secondtodo", completed: false},
--  {id: 3, body: "thirdtodo", completed: false},
--  {id: 4, body: "fourthtodo", completed: false},
--  {id: 5, body: "fifthtodo", completed: false},
--]
--
--function taskSetup(app){
--
--//index route
--app.get("/tasks", function (req, res){
--  res.json(tasks);
--});
--
--//new route
--app.get("/tasks/new", function (req, res){
--  res.send("new");
--});
--
--
--//show route
--app.get("/tasks/:id", function (req, res){
--  for(var a = 0; a < tasks.length; a++){
--    if (tasks[a].id == req.params.id){
--      res.json(tasks[a])
--    }
--  }
--});
--
--//create route
--app.post("/tasks", function (req, res){
--  var newTask = {id: req.body.id, body: req.body.body, completed: req.body.completed}
--  tasks.push(newTask)
--  res.send(newTask);
--  res.json(newTask);
--});
--
--//edit route
--app.get("tasks/:id/edit", function (req, res){
--  res.send("edit");
--});
--
--//update route
--app.patch("/tasks/:id", function (req, res){
--  res.send("update");
--});
--
--//delete route
--app.delete("/tasks/:id", function (req, rees){
--  res.send("delete");
--});
--}
--//what in your page you are saving to a variable and how you will reference that in your index.js
--exports.taskSetup = taskSetup;
+var tasks = [
+  {id: 1, body: "firsttodo", completed: false},
+  {id: 2, body: "secondtodo", completed: false},
+  {id: 3, body: "thirdtodo", completed: false},
+  {id: 4, body: "fourthtodo", completed: false},
+  {id: 5, body: "fifthtodo", completed: false},
+]
+
+function taskSetup(app){
+
+//index route
+app.get("/tasks", function (req, res){
+  res.json(tasks);
+});
+
+//new route
+app.get("/tasks/new", function (req, res){
+  res.send("new");
+});
+
+//show route
+app.get("/tasks/:id", function (req, res){
+  for(var a = 0; a < tasks.length; a++){
+    if (tasks[a].id == req.params.id){
+      res.json(tasks[a])
+    }
+  }
+});
+
+//create route
+app.post("/tasks", function (req, res){
+  var newTask = {id: req.body.id, body: req.body.body, completed: req.body.completed}
+  tasks.push(newTask)
+  res.send(newTask);
+  res.json(newTask);
+});
+
+//edit route
+app.get("tasks/:id/edit", function (req, res){
+  res.send("edit");
+});
+
+//update route
+app.patch("/tasks/:id", function (req, res){
+  res.send("update");
+});
+
+//delete route
+app.delete("/tasks/:id", function (req, rees){
+  res.send("delete");
+});
+}
+//what in your page you are saving to a variable and how you will reference that in your index.js
+exports.taskSetup = taskSetup;
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
--var express = require("express")
--var app = express()
--var bodyParser = require("body-parser")
--
--app.set("view engine", "hbs")
--
--app.use(bodyParser.json())
--app.use(bodyParser.urlencoded({ extended: true }))
--
--var task_routes = require("./app/controllers/task");
--task_routes.taskSetup(app)
--
--app.listen(3000, function(){
--  console.log("Listening on port 3000");
--});
--
--app.get("/", function(request, response){
--  response.sendFile(__dirname + "/app/views/index.html");
--});
+var express = require("express")
+var app = express()
+var bodyParser = require("body-parser")
+
+app.set("view engine", "hbs")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var task_routes = require("./app/controllers/task");
+task_routes.taskSetup(app)
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
+
+app.get("/", function(request, response){
+  response.sendFile(__dirname + "/app/views/index.html");
+});
